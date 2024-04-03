b0dd16c951995fb65daa441d6dc090c3f69bf188
SergWebAcceptance <serg.web@acceptance.ltd>
2024-04-03 11:19:17 +0300

    Fixed root address

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
-import Header from "../component/Header";
-import Footer from "../component/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "@/public/scss/base.scss";
 import { DM_Sans } from "next/font/google";
 
