6900a19e5fa6c93a1809bc8b1517ab5430070a1f
SergWebAcceptance <serg.web@acceptance.ltd>
2024-04-03 12:40:06 +0300

    Added hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import "@/public/scss/home.scss";
+import HomeHero from "../components/home/HomeHero";
 
 
 export default function Home() {
   return (
     <>
+    <HomeHero/>
     </>
   );
 }
---
+++ src/components/home/HomeHero.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+
+function HomeHero() {
+  return (
+    <section className="home-hero">
+      <iframe
+        src="https://my.spline.design/YbsmvAJ4EDVCvhZ4vURpYjaR/"
+        frameborder="0"
+        width="100%"
+        height="100%"
+      ></iframe>
+      <div className="_container">
+        <div className="home-hero__body">
+          <h1>
+            Have a crypto project?
+            <br />
+            We will boost your marketing!
+          </h1>
+          <h2>All-in-one performance tracking platform</h2>
+          <div className="buttons">
+            <Link href="#" className="main-button">
+              Try now
+            </Link>
+            <Link href="#" className="secondary-button">
+              Learn more
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default HomeHero;
