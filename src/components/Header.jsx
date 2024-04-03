95b1e12ca75fa73c6df272a5be89a8c31bdd313b
SergWebAcceptance <serg.web@acceptance.ltd>
2024-04-03 12:19:07 +0300

    Added Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,5 +3,26 @@ import Link from "next/link";
 import "@/public/scss/header.scss";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="_container">
+        <div className="head-wrap">
+            <Link href="/">
+                <img alt="logo" src="/images/logo.svg" />
+            </Link>
+            <div className="header-actions">
+                <Link href="#" className="cart">
+                    <img alt="logo" src="/images/cart.svg" />
+                </Link>
+                <Link href="#" className="main-button">
+                    Sign in
+                </Link>
+                <Link href="#" className="manu-btn">
+                    <img alt="logo" src="/images/burger.svg" />
+                </Link>
+            </div>
+        </div>
+      </div>
+    </header>
+  );
 }
