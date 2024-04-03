faadd05f237b0026f71834b37b2d175b8699f631
konstantinrts <k.developer@acceptance.ltd>
2024-04-03 15:36:03 +0300

    Testing branch Konstantin #2

---
+++ src/components/home/HomeNeed.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+function HomeNeed() {
+  return (
+    <section className="home-need">
+      <div className="home-need__container _container">
+        <div className="home-need__body">
+            <div className="home-need__content">
+                <h2 className="home-need__title">Need assistance?</h2>
+                <div className="home-need__text">Our experts are ready to analyse your crypto business and provide a tailored solution. It's free!</div>
+                <Link href="#" className="home-need__link">Contact us</Link>
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default HomeNeed;
