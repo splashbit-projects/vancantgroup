8b821e0fe9cb9b8a4bf723af2ee17bd3bf6494b2
born2bemedia <158292249+born2bemedia@users.noreply.github.com>
2024-04-03 16:58:23 +0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import HomeHero from "../components/home/HomeHero";
 import HomeSecond from "../components/home/HomeSecond";
 import HomePercents from "../components/home/HomePercents";
 import HomeNeed from "../components/home/HomeNeed";
-
+import HomeWhy from "../components/home/HomeWhy";
 
 export default function Home() {
   return (
