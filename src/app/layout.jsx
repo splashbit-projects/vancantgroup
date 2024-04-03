c4f57d62ac172ec2b78f592bfe302d1a25725dda
SergWebAcceptance <serg.web@acceptance.ltd>
2024-04-03 11:17:40 +0300

    Fixed footer and header connection

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Header from "../component/Header";
+import Footer from "../component/Footer";
 import "@/public/scss/base.scss";
 import { DM_Sans } from "next/font/google";
 
