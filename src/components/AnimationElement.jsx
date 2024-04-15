8aed5cee84287bc5f4d893a3543b9e0081b12fff
SergWebAcceptance <serg.web@acceptance.ltd>
2024-04-15 17:08:49 +0300

    Fixed animation

--- src/components/AnimationElement.jsx
+++ src/components/AnimationElement.jsx
@@ -1,40 +1,12 @@
-import React, { Suspense, lazy, useState, useEffect, useRef } from "react";
+"use client";
+import React, { Suspense, lazy } from "react";
 const Spline = lazy(() => import("@splinetool/react-spline"));
 
 function AnimationElement({ animation }) {
-  const [loadAnimation, setLoadAnimation] = useState(false);
-  const containerRef = useRef(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting) {
-          setLoadAnimation(true);
-          observer.disconnect(); // Stop observing once loaded
-        }
-      },
-      {
-        rootMargin: '50px', // Load slightly before the element comes into view
-      }
-    );
-
-    if (containerRef.current) {
-      observer.observe(containerRef.current);
-    }
-
-    return () => observer.disconnect(); // Clean up observer on component unmount
-  }, []);
-
   return (
-    <div ref={containerRef}>
-      {loadAnimation ? (
-        <Suspense fallback={<div>Loading...</div>}>
-          <Spline scene={animation} />
-        </Suspense>
-      ) : (
-        <div style={{ height: '500px', background: 'grey' }}>Waiting for load...</div> // Placeholder for layout shift prevention
-      )}
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Spline scene={animation} />
+    </Suspense>
   );
 }
 
