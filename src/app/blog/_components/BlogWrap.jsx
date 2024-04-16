027b6a1897c4fcdf31c9e3c5cd81721a50cbc7a7
SergWebAcceptance <serg.web@acceptance.ltd>
2024-04-16 20:52:30 +0300

    Fixed blog

--- src/app/blog/_components/BlogWrap.jsx
+++ src/app/blog/_components/BlogWrap.jsx
@@ -10,10 +10,14 @@ function BlogWrap() {
         <div className="blog-wrap__body">
           <div className="left">
             <RevealWrapper origin="bottom">
-              <img src="/images/blog.png" />
+              <Link href="/blog/maximising-roi-effective-strategies-for-optimising-ppc-campaigns-in-crypto-marketing">
+                <img src="/images/blog.png" />
+              </Link>
               <h2>
-                Maximising ROI: Effective Strategies for Optimising PPC
-                Campaigns in Crypto Marketing
+                <Link href="/blog/maximising-roi-effective-strategies-for-optimising-ppc-campaigns-in-crypto-marketing">
+                  Maximising ROI: Effective Strategies for Optimising PPC
+                  Campaigns in Crypto Marketing
+                </Link>
               </h2>
               <p>
                 Learn how to supercharge your crypto marketing efforts with
@@ -21,7 +25,10 @@ function BlogWrap() {
                 leads, and maximise ROI from your PPC campaigns in the dynamic
                 world of cryptocurrency marketing.
               </p>
-              <Link href="/blog/maximising-roi-effective-strategies-for-optimising-ppc-campaigns-in-crypto-marketing" className="main-button">
+              <Link
+                href="/blog/maximising-roi-effective-strategies-for-optimising-ppc-campaigns-in-crypto-marketing"
+                className="main-button"
+              >
                 Read
               </Link>
             </RevealWrapper>
@@ -29,28 +36,43 @@ function BlogWrap() {
           <div className="right">
             <RevealWrapper origin="bottom">
               <h2>
-                Navigating Regulatory Challenges: Compliance Tips for Crypto
-                Advertising
+                <Link href="/blog/maximising-roi-effective-strategies-for-optimising-ppc-campaigns-in-crypto-marketing">
+                  Navigating Regulatory Challenges: Compliance Tips for Crypto
+                  Advertising
+                </Link>
               </h2>
-              <Link href="/blog/navigating-regulatory-challenges-compliance-tips-for-crypto-advertising" className="main-button">
+              <Link
+                href="/blog/navigating-regulatory-challenges-compliance-tips-for-crypto-advertising"
+                className="main-button"
+              >
                 Read
               </Link>
             </RevealWrapper>
             <RevealWrapper origin="bottom">
               <h2>
-                Harnessing the Power of Influencer Marketing in Cryptocurrency
-                Promotion
+                <Link href="/blog/maximising-roi-effective-strategies-for-optimising-ppc-campaigns-in-crypto-marketing">
+                  Harnessing the Power of Influencer Marketing in Cryptocurrency
+                  Promotion
+                </Link>
               </h2>
-              <Link href="/blog/harnessing-the-power-of-influencer-marketing-in-cryptocurrency-promotion" className="main-button">
+              <Link
+                href="/blog/harnessing-the-power-of-influencer-marketing-in-cryptocurrency-promotion"
+                className="main-button"
+              >
                 Read
               </Link>
             </RevealWrapper>
             <RevealWrapper origin="bottom">
               <h2>
-                Crypto Content Marketing: Crafting Engaging Content for Digital
-                Currency Audiences
+                <Link href="/blog/maximising-roi-effective-strategies-for-optimising-ppc-campaigns-in-crypto-marketing">
+                  Crypto Content Marketing: Crafting Engaging Content for
+                  Digital Currency Audiences
+                </Link>
               </h2>
-              <Link href="/blog/crypto-content-marketing-crafting-engaging-content-for-digital-currency-audiences" className="main-button">
+              <Link
+                href="/blog/crypto-content-marketing-crafting-engaging-content-for-digital-currency-audiences"
+                className="main-button"
+              >
                 Read
               </Link>
             </RevealWrapper>
