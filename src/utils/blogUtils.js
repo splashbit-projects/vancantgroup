54f389dda690511882213fd7b1100c8ca536e4f2
SergWebAcceptance <serg.web@acceptance.ltd>
2024-04-16 19:14:42 +0300

    Fixed blog content

--- src/utils/blogUtils.js
+++ src/utils/blogUtils.js
@@ -3,7 +3,7 @@ import matter from "gray-matter";
 import { marked } from "marked";
 
 export async function getPost(slug) {
-    const text = await readFile(`./src/content/${slug}.md`, "utf8");
+    const text = await readFile(`./content/${slug}.md`, "utf8");
     const { content, data: { title, seo_title, seo_desctiption } } = matter(text);
     const body = marked(content);
     return {slug, title, seo_title, seo_desctiption, body}
