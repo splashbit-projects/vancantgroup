046cdb6f63e4b24705284dfbb529fb4aed1633e3
SergWebAcceptance <serg.web@acceptance.ltd>
2024-04-16 20:47:57 +0300

    Fixed hydration

--- src/components/LayoutElements/AnimationElement.jsx
+++ src/components/LayoutElements/AnimationElement.jsx
@@ -1,13 +1,15 @@
-'use client';
-import React, { Suspense, lazy, useState, useEffect } from 'react';
-const Spline = React.lazy(() => import('@splinetool/react-spline'));
+"use client";
+import React, { Suspense, lazy, useState, useEffect } from "react";
+const Spline = React.lazy(() => import("@splinetool/react-spline"));
 
 function AnimationElement({ animation, fallbackImage, mobileHidden }) {
   const [isAndroidMobile, setIsAndroidMobile] = useState(false);
-  const [isMobileWidth, setIsMobileWidth] = useState(typeof window !== 'undefined' && window.innerWidth < 767);
+  const [isMobileWidth, setIsMobileWidth] = useState(
+   false
+  );
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       const userAgent = navigator.userAgent || navigator.vendor || window.opera;
       if (/android/i.test(userAgent)) {
         setIsAndroidMobile(true);
@@ -16,31 +18,29 @@ function AnimationElement({ animation, fallbackImage, mobileHidden }) {
   }, []);
 
   useEffect(() => {
+    setIsMobileWidth(window.innerWidth < 767);
     function handleResize() {
       setIsMobileWidth(window.innerWidth < 767);
     }
+    
+    window.addEventListener("resize", handleResize);
 
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   if (isMobileWidth) {
-    if (mobileHidden){
+    if (mobileHidden) {
       return <></>;
     } else {
       return <img src={fallbackImage} alt="Fallback" className="animation" />;
     }
-    
+  } else {
+    return (
+      <Suspense fallback={<div>...</div>}>
+        <Spline className="animation" scene={animation} />
+      </Suspense>
+    );
   }
-
-  
-
-  return (
-    <Suspense fallback={<div>...</div>}>
-      <Spline className="animation" scene={animation} />
-    </Suspense>
-  );
 }
 
 export default AnimationElement;
