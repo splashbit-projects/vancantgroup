de2ec6d3c9fa93b50c1bcab50adae96f2d069183
SergWebAcceptance <serg.web@acceptance.ltd>
2025-05-12 14:00:43 +0300

    changed facebook

--- src/app/[locale]/_global-components/LayoutElements/socials/Facebook.jsx
+++ src/app/[locale]/_global-components/LayoutElements/socials/Facebook.jsx
@@ -3,7 +3,7 @@ import { Link } from "@/src/navigation";
 
 function Facebook() {
   return (
-    <Link href="https://www.facebook.com/vancantgroupltd/" target="_blank">
+    <Link href="https://www.facebook.com/vancantgroupcom/" target="_blank">
       <img alt="facebook" src="/images/facebook.svg" />
     </Link>
   );
